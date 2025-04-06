c859d0dc7472a686b497b4c573d161e836fb3447
NirvanaMangion <151510573+NirvanaMangion@users.noreply.github.com>
2025-04-06 15:37:58 +0200

    front page

--- app/src/main/java/com/nirvana/gymapp/MainActivity.kt
+++ app/src/main/java/com/nirvana/gymapp/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.nirvana.gymapp
 
+import android.graphics.Color
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // Set colored "Strive"
+        val titleView = findViewById<TextView>(R.id.striveTitle)
+        val title = SpannableString("Strive")
+        title.setSpan(ForegroundColorSpan(Color.parseColor("#FFD600")), 0, 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        titleView.text = title
+
+        // Set "Already have an account? Log in" with Log in in purple
+        val loginView = findViewById<TextView>(R.id.loginText)
+        val loginText = SpannableStringBuilder("Already have an account? Log in")
+        val loginStart = loginText.indexOf("Log in")
+        val loginEnd = loginStart + "Log in".length
+        loginText.setSpan(
+            ForegroundColorSpan(Color.parseColor("#AA88FF")),
+            loginStart, loginEnd,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        loginView.text = loginText
     }
 }
